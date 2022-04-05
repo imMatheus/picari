abf52c11ed44e429df1e212eaa7407785d92ce64
Nabil <hayeknabil214@gmail.com>
2022-04-05 19:52:48 +0200

    camera support check & discord auth changes

--- src/routes/discord/index.ts
+++ src/routes/discord/index.ts
@@ -1,40 +1,58 @@
 export async function get(req) {
-	const code = req.url.searchParams.get('code');
-	const res = await (
-		await fetch('https://discord.com/api/oauth2/token', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			body: new URLSearchParams({
-				client_id: import.meta.env.VITE_DISCORD_CLIENT_ID.toString(),
-				client_secret: import.meta.env.VITE_DISCORD_OAUTH_SECRET.toString(),
-				grant_type: 'authorization_code',
-				redirect_uri: import.meta.env.VITE_BASE_URL + '/discord',
-				code: code.toString()
+	let user;
+	try {
+		const code = req.url.searchParams.get('code');
+		const token = await (
+			await fetch('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				body: new URLSearchParams({
+					client_id: import.meta.env.VITE_DISCORD_CLIENT_ID as string,
+					client_secret: import.meta.env.VITE_DISCORD_OAUTH_SECRET as string,
+					grant_type: 'authorization_code' as string,
+					redirect_uri: (import.meta.env.VITE_BASE_URL + '/discord') as string,
+					code: code as string
+				})
 			})
-		})
-	).json();
+		).json();
 
-	const userData = await (
-		await fetch('https://discord.com/api/oauth2/@me', {
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${res.access_token}`
+		user = await (
+			await fetch('https://discord.com/api/users/@me', {
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${token.access_token}`
+				}
+			})
+		).json();
+	} catch (err) {
+		return {
+			status: 500,
+			body: {
+				error: 'Internal server error'
 			}
-		})
-	).json();
+		};
+	}
 
-	// if (!userData.user.email)
-	// 	return {
-	// 		status: 400,
-	// 		body: {
-	// 			error: 'Discord account not verified by email'
-	// 		}
-	// 	};
+	if (!user.id)
+		return {
+			status: 400,
+			body: {
+				error: 'User could not be fetched'
+			}
+		};
+
+	if (!user.email)
+		return {
+			status: 400,
+			body: {
+				error: 'User does not have a email'
+			}
+		};
 
 	return {
 		status: 200,
-		body: await userData
+		body: user
 	};
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
 	import Feed from '$components/Feed.svelte';
+	import { onMount } from 'svelte';
+
+	let resolveCameraSupported;
+	let cameraSupported = new Promise(function (resolve) {
+		resolveCameraSupported = resolve;
+	});
+
+	onMount(() => {
+		resolveCameraSupported('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices);
+		navigator.mediaDevices.getUserMedia({ video: true });
+	});
 </script>
 
-<div class="mx-auto max-w-3xl p-3 text-gray-900">
-	<Feed />
-</div>
+{#await cameraSupported}
+	<p>Loading ...</p>
+{:then isCameraSupported}
+	{#if isCameraSupported}
+		<div class="mx-auto max-w-3xl p-3 text-gray-900">
+			<Feed />
+		</div>
+	{:else}
+		<p>Camera is not supported on your device</p>
+	{/if}
+{/await}
 
 <style>
 </style>
