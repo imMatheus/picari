2b5daef12441600238b1c13883ee635f8ac0ca31
imMatheus <matheusmendesbarata03@gmail.com>
2022-04-05 18:07:51 +0200

    added card reactions

---
+++ src/lib/components/FeedReactionsChip.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let emoji: string;
+	export let numberOfReactions: number;
+</script>
+
+<div
+	class="bg-gray-900 cursor-pointer text-gray-200 py-0 px-2 flex items-center gap-1 rounded-full border-2 border-gray-900 hover:border-theme"
+>
+	{emoji}
+	<span class="text-xs">
+		{numberOfReactions}
+	</span>
+</div>
+
+<style>
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import Feed from '../lib/components/Feed.svelte';
-	import FeedCard from '../lib/components/FeedCard.svelte';
 </script>
 
-<div class="mx-auto max-w-4xl p-3 text-gray-900">
+<div class="mx-auto max-w-3xl p-3 text-gray-900">
 	<Feed />
 </div>
 
