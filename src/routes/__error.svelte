4aa5221d14fa44517db8a5a2e8cd068010a3eab7
imMatheus <matheusmendesbarata03@gmail.com>
2022-04-17 22:25:08 +0200

    added 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,6 @@
+<div class="flex h-screen flex-col items-center justify-center space-y-4 p-5">
+	<h1 class="text-center text-5xl font-bold">404</h1>
+	<div class="w-max">
+		<a href="/" role="button" class="rounded-md bg-theme py-1.5 px-5 text-xl text-white">Go back</a>
+	</div>
+</div>
